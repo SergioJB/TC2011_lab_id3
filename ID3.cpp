82d5337cab9ca41dfc9c05b31f449d2c5edc0983
Sergio Juarez <a01270410@itesm.mx>
2016-11-11 12:43:34 -0600

    printing the tree solved

--- ID3.cpp
+++ ID3.cpp
@@ -21,7 +21,7 @@ struct node
 	struct node* parent;
 	std::vector<struct node*> childrens;
 	string name;
-	int type;
+	int type; // 0:attr ; 1:value; 2:answer;
 	bool answer;
 
 };
@@ -33,7 +33,9 @@ double log2( double number ) {
 double entropy(attributeInnerMap Set){
 	double sum = 0.0;
 	for(std::map<string, int>::iterator ite = Set.second.begin();ite != Set.second.end(); ite++){
-		sum -= (double) ite->second / Set.first * log2((double) ite->second / Set.first);
+		if(ite->second > 0){
+			sum -= (double) ite->second / Set.first * log2((double) ite->second / Set.first);
+		}
 	}
 	return sum;
 }
@@ -47,37 +49,139 @@ double infoGain(attributeInnerMap Set,vector<attributeInnerMap> SubSetV){
 
 }
 
-int toSplit(vector<string> index, map<string, vector<string> > dictionary, vector<vector<string> > samples, string action, vector<string> values){
+bool doNeedSplit(vector<string> index, map<string, vector<string> > dictionary, vector<vector<string> > samples){
+	attributeInnerMap Set;
+	int answerIndex = index.size()-1;
+	Set.first=samples.size();
+	for (int i = 0; i < dictionary[index[answerIndex]].size(); i++)
+	{
+		int count=0;
+		for(int j = 0; j < samples.size(); j++){
+			if(samples[j][answerIndex] == dictionary[index[answerIndex]][i]){
+				count++;
+			}
+		}
+		Set.second[dictionary[index[answerIndex]][i]] = count;
+	}
+
+	if(entropy(Set)!=0.0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+int toSplit(vector<string> index, map<string, vector<string> > dictionary, vector<vector<string> > samples){
 	int instances = samples.size();
 	vector<attributeInnerMap> SubSetV;
 	attributeInnerMap Set, aux;
 	Set.first=instances;
+	int answerIndex = index.size()-1;
 	double maxGain = -1.0;
 	int indexInt = -1;
-	for(int i = 0; i < index.size()-1; i++){
-		int inst=0;
-		for (int j = 0; j < dictionary[index[i]].size(); j++)
-		{
-			int count = 0;
-			for (int k = 0; k < samples.size(); k++)
-			{
-				if(samples[k][i]==dictionary[index[i]][j]){
-					count++;
-				}
+	for (int i = 0; i < dictionary[index[answerIndex]].size(); i++)
+	{
+		int count=0;
+		for(int j = 0; j < samples.size(); j++){
+			if(samples[j][answerIndex] == dictionary[index[answerIndex]][i]){
+				count++;
 			}
-			aux.second[dictionary[index[i]][j]] = count;
-			inst+=count;
 		}
-		aux.first = inst;
+		Set.second[dictionary[index[answerIndex]][i]] = count;
+		// cout << dictionary[index[answerIndex]][i] << " " << count << endl;
 	}
 
+	// cout << "Set: " << instances << endl;
+	// cout << "entropy: " << entropy(Set) << endl;
 
+
+	for(int i = 0; i < index.size()-1; i++){
+		for (int j = 0; j < dictionary[index[i]].size(); j++)
+		{
+			int inst = 0;
+			for (int k = 0; k < dictionary[index[answerIndex]].size(); k++)
+			{
+				int count = 0;
+				for (int n = 0; n < samples.size(); n++)
+				{
+					if(samples[n][answerIndex] == dictionary[index[answerIndex]][k] && samples[n][i] == dictionary[index[i]][j]){
+						count++;
+					}
+				}
+				aux.second[dictionary[index[answerIndex]][k]] = count;
+				// cout << dictionary[index[answerIndex]][k] << " " << count << endl;
+				inst+=count;
+			}
+			// cout << dictionary[index[i]][j] << ": " << inst <<endl;
+			aux.first = inst;
+			SubSetV.push_back(aux);
+			aux.second.clear();
+		}
+		double gainN = infoGain(Set,SubSetV);
+		SubSetV.clear();
+		// cout << gainN << endl;
+		if(gainN > maxGain){
+			maxGain = gainN;
+			indexInt = i;
+		}
+	}
+
+	// cout << "MaxGain: " << maxGain << endl;
+	// cout << index[indexInt] << endl;
+	return indexInt;
 }
+
+void splitting(struct node* Node,vector<string> index, map<string, vector<string> > dictionary, vector<vector<string> > samples, string level){
+	if(doNeedSplit(index,dictionary,samples)){
+		int indexSplit = toSplit(index,dictionary,samples);
+		vector<string> copyIndex;
+		map<string, vector<string> > copyDictionary;
+		vector<vector<string> > branchSamples;
+		Node->name = index[indexSplit];
+		Node->type = 0; //attribute
+		Node->answer = false;
+		for (int i = 0; i < dictionary[index[indexSplit]].size(); i++){
+			struct node* child = new node;
+			child->type=1; //att val
+			child->parent=Node;
+			child->name=dictionary[index[indexSplit]][i];
+			child->answer=false;
+			cout << level << Node->name << ": " << child->name << endl;
+			Node->childrens.push_back(child);
+
+			copyIndex = index;
+
+			copyIndex.erase(copyIndex.begin()+(indexSplit));
+			for(int j=0;j<samples.size();j++){
+				if(samples[j][indexSplit]==dictionary[index[indexSplit]][i]){
+					branchSamples.push_back(samples[j]);
+				}
+			}
+			for(int j=0;j<branchSamples.size();j++){
+				branchSamples[j].erase(branchSamples[j].begin()+indexSplit);
+			}
+
+			splitting(child,copyIndex,dictionary,branchSamples,"  ");
+			copyIndex.clear();
+			branchSamples.clear();
+		}
+	}else{
+		int indexAnswer = index.size() - 1;
+		struct node* child = new node;
+		child->type=2; //answer val
+		child->parent=Node;
+		child->name=samples[0][indexAnswer];
+		child->answer=true;
+		cout << level << "  ANSWER: " << child->name << endl;
+		Node->childrens.push_back(child);
+	}
+}
+
 
 class TreeID3{
 
 private:
-	struct node *root;
+	struct node* root;
 	vector<string> samplesT;
 	vector<string> index;
 	map<string, vector<string> > dictionary;
@@ -114,7 +218,10 @@ public:
 		}
 	}
 
-
+	void createTree(){
+		// toSplit(index,dictionary,Samples);;
+		splitting(root,index,dictionary,Samples,"");
+	}
 
 };
 
@@ -154,7 +261,7 @@ int main(int argc, char *argv[]){
 	        extract >> recov;
 	        aux = recov;
 	        indexAttribute.push_back(aux);
-	        std::cout << "Attribute: " << aux << std::endl;
+	        // std::cout << "Attribute: " << aux << std::endl;
 	        buffer = "";
 	        while(extract >> recov){
 	            buffer += recov;
@@ -169,26 +276,26 @@ int main(int argc, char *argv[]){
 	            }
 	        }
 	        
-	        cout << buffer << std::endl;
+	        // cout << buffer << std::endl;
 	        stringstream extract(buffer);
 
 	        while(extract >> recov){
 	            auxVectorString.push_back(recov);
-	            cout << "Pushing '" << recov << "' in the vector\n";
+	            // cout << "Pushing '" << recov << "' in the vector\n";
 	        }
 
-	        cout << "Putting the Attribute " << aux << " in dictionaryMap with all its values\n";
+	        // cout << "Putting the Attribute " << aux << " in dictionaryMap with all its values\n";
 
 	        dictionaryMap[aux]=auxVectorString;
 
-	        cout << "checking...\n\n";
+	        // cout << "checking...\n\n";
 
-	        for (int i = 0; i < dictionaryMap[aux].size(); i++)
-	        {
-	        	cout << dictionaryMap[aux][i] << ' ';
-	        }
+	        // for (int i = 0; i < dictionaryMap[aux].size(); i++)
+	        // {
+	        // 	cout << dictionaryMap[aux][i] << ' ';
+	        // }
 
-	        cout << "\n\n";
+	        // cout << "\n\n";
 
 	        while(!auxVectorString.empty()){
 	        	auxVectorString.pop_back();
@@ -213,7 +320,7 @@ int main(int argc, char *argv[]){
 		    		continue;
 		    	}else{
 					samplesTrainings.push_back(buffer);
-					cout << buffer << std::endl;
+					//cout << buffer << std::endl;
 				}
 		    }
 
@@ -224,7 +331,9 @@ int main(int argc, char *argv[]){
 
 	TreeID3 tree(samplesTrainings,indexAttribute,dictionaryMap);
 
-	std::cout << "finish...\n" << std::endl;
+	tree.createTree();
+
+	//std::cout << "finish...\n" << std::endl;
 
 	// cout << "test of the entropy and info gain\n";
 	// cout << "theorical Entropy(S) = - (9/14) Log2 (9/14) - (5/14) Log2 (5/14) = 0.940\n";
